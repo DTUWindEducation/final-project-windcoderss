d93c739088656bbd3d67dea6dfbde9aeef6b09fb
Ida Markussen <s240387@dtu.dk>
2025-04-19 11:57:35 +0200

    Load and parse multiple provided netCDF4 files.

--- examples/main.py
+++ examples/main.py
@@ -4,5 +4,9 @@ winddata_file_path = "./inputs/1997-1999.nc"
 winddata1 = final_project.WindData(winddata_file_path)
 
 latitudes1 = winddata1.get_latitude()
+longitudes1 = winddata1.get_longitudes()
 
-print(latitudes1)
+component_name = ['u10', 'v10', 'u100', 'v100']
+wind_data1 = winddata1.get_components_of_wind(component_name)
+print("Wind Data:", wind_data1['u10'])
+
--- src/final_project/__init__.py
+++ src/final_project/__init__.py
@@ -37,3 +37,42 @@ class WindData:
             latitudes_out[i] = latitudes[i]
 
         return latitudes_out
+    
+    def get_longitudes(self):
+        """
+        Retrieves the latitude values from the NetCDF dataset.
+        This method accesses the 'latitude' variable in the NetCDF file and 
+        extracts its values into a NumPy array.
+        Returns:
+            numpy.ndarray: A NumPy array containing the latitude values.
+        """
+
+        rootgrp = self.get_rootgrp()
+        longitudes = rootgrp.variables['longitude']
+        longitudes_out = np.zeros(len(longitudes))
+        for i in range(len(longitudes)):
+            longitudes_out[i] = longitudes[i]
+
+        return longitudes_out
+    
+
+    
+    def get_components_of_wind(self, component_name):
+        wind_data = {}
+
+        latitudes = self.get_latitude()
+        longitudes = self.get_longitudes()
+
+        rootgrp = self.get_rootgrp()
+
+        wind_data['locations'] = np.array([[latitudes[0], longitudes[0]],
+                                      [latitudes[1], longitudes[0]],
+                                      [latitudes[0], longitudes[1]],
+                                      [latitudes[1], longitudes[1]]])
+        
+        for i in range(len(component_name)):
+            data = rootgrp.variables[component_name[i]]
+            wind_data[component_name[i]] = np.array([data[:, 0, 0], data[:, 1, 0], data[:, 0, 1], data[:, 1, 1]])
+
+        return wind_data
+
